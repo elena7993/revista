cf5f3ccc86159417ac4d230ce7857031471f943f
elena7993 <elenalee7993@gmail.com>
2024-11-25 19:17:47 +0900

    25_11_24

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,33 +5,44 @@ const Wrapper = styled.div`
   width: 100%;
   max-width: 372px;
   height: 100vh;
-  background: url("/northern_lights_ealistic.jpg") center no-repeat / cover;
   padding: 0 ${mainStyle.Padding_main};
+  background: url("/northernlights_realistic.jpg") center no-repeat / cover;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
   border: 1px solid #dbdbdb;
   margin: 0 auto;
-  h2 {
-    font-size: 42px;
-  }
-  p {
-    font-size: 18px;
+  .title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h2 {
+      font-size: 42px;
+    }
+    p {
+      font-size: 18px;
+    }
   }
   button {
     all: unset;
     width: 150px;
     height: 50px;
     border: 1px solid #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
 const Home = () => {
   return (
     <Wrapper>
-      <h2>Revista</h2>
-      <p>Here, your dream becomes a story</p>
+      <div className="title">
+        <h2>Revista</h2>
+        <p>Here, your dream becomes a story</p>
+      </div>
+
       <div>
         <button>Start Dreaming</button>
       </div>
